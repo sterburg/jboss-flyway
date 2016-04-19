35f020fd99634fae8cc0532b9289966dea81e04b
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2016-04-19 17:18:36 +0200

    fix locations

--- flyway.sh
+++ flyway.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf /usr/local/flyway-4.0/sql
-ln -s /var/git/jboss-greeter/src/main/resources/sql /usr/local/flyway-4.0/sql
+ln -s /var/git/jboss-greeter/src/main/resources/sql /usr/local/flyway-4.0/
 cd /usr/local/flyway-4.0/sql
 
 /usr/local/flyway-4.0/flyway $* \
