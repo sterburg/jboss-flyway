221ad21338f380306bd4a859d42b96b48ce08037
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2016-04-19 12:09:56 +0200

    hack

--- flyway.sh
+++ flyway.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -rf /usr/local/flyway-4.0/sql
+ln -s /var/git/jboss-greeter/src/main/resources/sql /usr/local/flyway-4.0/sql
+
 /usr/local/flyway-4.0/flyway $* \
   -user=${MY_USERNAME} \
   -password=${MY_PASSWORD} \
