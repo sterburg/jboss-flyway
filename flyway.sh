9ab975a340f6367f275fe88cc048426cb0494b1b
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2016-04-19 07:58:48 +0200

    fix schema(s)

--- flyway.sh
+++ flyway.sh
@@ -3,6 +3,6 @@
 /usr/local/flyway-4.0/flyway $* \
   -user=${MY_USERNAME} \
   -password=${MY_PASSWORD} \
-  -schema=${MY_DATABASE} \
+  -schemas=${MY_DATABASE} \
   -target=${OPENSHIFT_BUILD_REFERENCE/v/}
 
